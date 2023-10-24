c257d6182bce00f18192670bbd99fea3b891a009
elecun <elecun2@gmail.com>
2023-10-24 22:54:36 +0900

    updated

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 from typing import Union
-
+from celery import Celery
 from fastapi import FastAPI
 
 app = FastAPI()
 
+celery = Celery(
+    __name__,
+    broker="redis://127.0.0.1:6379/0",
+    backend="redis://127.0.0.1:6379/0"
+)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+async def root():
+    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@celery.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
