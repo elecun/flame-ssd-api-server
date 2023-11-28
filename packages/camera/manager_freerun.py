a7ac35ff3866cdd7fe0bd6288fc15ac95f2892d2
elecun <elecun2@gmail.com>
2023-11-28 14:19:46 +0900

    fixed

--- packages/camera/manager_freerun.py
+++ packages/camera/manager_freerun.py
@@ -13,9 +13,12 @@ import argparse
 import pathlib
 from module import camera_module
 import cv2
-import timeit
+from datetime import datetime
 import signal
 from joblib import Parallel, delayed, parallel_backend
+import redis
+import numpy as np
+import h5py
 
 
 WORKING_PATH = pathlib.Path(__file__).parent
@@ -33,6 +36,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', nargs='?', required=False, type=str, help="Configuration file")
     parser.add_argument('--show', nargs='?', default="true", required=False, help="Display Image on Window")
+    parser.add_argument('--test_file_saving', nargs='?', default=False, required=False, help="Testing for saving image file from memory data")
+    parser.add_argument('--test_redis_saving', nargs='?', default=False, required=False, help="Testing for saving to redis from memory data")
+    parser.add_argument('--test_convert_hdf5_from_memory', nargs='?', default=False, required=False, help="Testing for Converting images to HDF5")
     args = parser.parse_args()
 
     try:
@@ -65,7 +71,7 @@ if __name__ == "__main__":
 
                 print("Using device ", camera.GetDeviceInfo().GetFullName())
                 
-
+        # select one
         _camera_container.StartGrabbing(pylon.GrabStrategy_LatestImageOnly, pylon.GrabLoop_ProvidedByUser)
         #_camera_container.StartGrabbing(pylon.GrabStrategy_OneByOne, pylon.GrabLoop_ProvidedByUser)
         #_camera_container.StartGrabbing(pylon.GrabStrategy_UpcomingImage, pylon.GrabLoop_ProvidedByUser)
@@ -82,42 +88,41 @@ if __name__ == "__main__":
         _test_count = 0
         _store_path = WORKING_PATH / "capture"
         _store_path.mkdir(parents=True, exist_ok=True)
-        _t_start = timeit.default_timer()
+        _t_total_start = datetime.now()
         _store_buffer = {}
         _store_buffer[0] = []
         _store_buffer[1] = []
         while _camera_container.IsGrabbing():
             print(f"Test Count :{_test_count}")
-            if _test_count==390:
+            if _test_count==(30*13): # 13 sec x 30fps 
                 print("Test Done")
                 break
 
-            for idx, camera in enumerate(_camera_container):
-                # t_start = timeit.default_timer()
-                _grab_result[idx] = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+            for cam_idx, camera in enumerate(_camera_container):
+                _grab_result[cam_idx] = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
                 _fps = 0
 
-                if _grab_result[idx].GrabSucceeded():
-                    image = _converter.Convert(_grab_result[idx])
+                if _grab_result[cam_idx].GrabSucceeded():
+                    image = _converter.Convert(_grab_result[cam_idx])
                     raw_image = image.GetArray()
 
                     # for test (file saving)
-                    #cv2.imwrite(str(_store_path/f"{idx}_{_test_count}.png"), raw_image)
+                    #cv2.imwrite(str(_store_path/f"{cam_idx}_{_test_count}.png"), raw_image)
 
                     # for test (memory buffer)
-                    _store_buffer[idx].append(raw_image)
+                    _store_buffer[cam_idx].append(raw_image)
 
                 if _show==True:
-                    _now_tick = _grab_result[idx].GetTimeStamp()
+                    _now_tick = _grab_result[cam_idx].GetTimeStamp()
                     if len(_grab_tick)==_camera_container.GetSize():
-                        _fps = 1/((_now_tick - _grab_tick[idx])*CAMERA_TICK_TIME/1000000000)
+                        _fps = 1/((_now_tick - _grab_tick[cam_idx])*CAMERA_TICK_TIME/1000000000)
                         cv2.putText(raw_image, f"fps:{_fps:.1f}", (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2, cv2.LINE_AA)
                     _grab_tick[idx] = _now_tick
                 
-                    cv2.namedWindow(f"{idx}", cv2.WINDOW_AUTOSIZE)
-                    cv2.imshow(f"{idx}", raw_image)
+                    cv2.namedWindow(f"{cam_idx}", cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow(f"{cam_idx}", raw_image)
 
-                _grab_result[idx].Release()
+                _grab_result[cam_idx].Release()
             
             _test_count+=1 # increate count
             
@@ -126,16 +131,51 @@ if __name__ == "__main__":
                 break
         
         # save image to file from memory buffer
-        def test(k):
-            for idx, image in enumerate(_store_buffer[k]):
-                print(f"saving {k}-{idx}")
-                cv2.imwrite(str(_store_path/f"{k}_{idx}.png"), image)
+        def test_saving_file(cam_idx):
+            for idx, image in enumerate(_store_buffer[cam_idx]):
+                print(f"saving {cam_idx}-{idx}")
+                cv2.imwrite(str(_store_path/f"{cam_idx}_{idx}.png"), image)
+
 
         # save image to file from memory buffer with Parallelism
-        Parallel(n_jobs=8, prefer="threads")(delayed(test)(k) for k in _store_buffer)
+        if args.test_file_saving:
+            _t_start_test_file_saving = datetime.now()
+            Parallel(n_jobs=8, prefer="threads")(delayed(test_saving_file)(k) for k in _store_buffer)
+            _t_end_test_file_saving = datetime.now()
+            print(f"Elapsed Time (storing into redis) : {(_t_start_test_file_saving-_t_end_test_file_saving).total_seconds()}")
 
-        _t_end = timeit.default_timer()
-        print(f"Elapsed Time : {(_t_end-_t_start)}")
+        elif args.test_redis_saving:
+            _t_start_test_redis_saving = datetime.now()
+            try:
+                redis_handle = redis.StrictRedis(host='localhost', port=6379)
+                for cam_idx, camera_buffer in enumerate(_store_buffer):
+                    for idx, image in camera_buffer:
+                        img_bytes = np.array2string(image)
+                        redis_handle.set(f"camera{cam_idx}_{idx}",img_bytes)
+
+                redis_handle.close()
+            except Exception as e:
+                print(e)
+            
+            _t_end_test_redis_saving = datetime.now()
+            print(f"Elapsed Time (storing into redis) : {(_t_start_test_redis_saving-_t_end_test_redis_saving).total_seconds()}")
+
+        elif args.test_convert_hdf5_from_memory:
+            try:
+                with h5py.File("hdf5_full", 'a') as hf:
+                    group = hf.create_group("dataset")
+                    for cam_idx, camera_buffer in enumerate(_store_buffer):
+                        for idx, image in camera_buffer:
+                            image_np = np.asarray(image)
+                            dset = group.create_dataset(f"camera{cam_idx}_{idx}", data=image_np)
+                hf.close()
+
+            except Exception as e:
+                print(e)
+
+        _t_total_end = datetime.now()
+        print(f"Elapsed Time : {(_t_total_end-_t_total_start).total_seconds()}")
+
             
         _camera_container.StopGrabbing()
         _camera_container.Close()
